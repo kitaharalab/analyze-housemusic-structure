a3b86e1407910ec3b0138942134890f17d85caf5
Jtwulf <jtwulf1005@icloud.com>
2023-12-20 23:42:09 +0900

    add process_mode combined or single

--- src/experiment4.py
+++ src/experiment4.py
@@ -6,7 +6,7 @@ def load_section_data(json_path: str):
         data = json.load(file)
     return data
 
-def process_midi_file(midi_path, section_data, drum_mapping):
+def process_midi_file_single(midi_path, section_data, drum_mapping):
     visualizer = DrumMidiVisualizer(midi_path)
     drum_events = visualizer.get_drum_events()
     section_counts = {section['label']: {name: 0 for name in drum_mapping.values()} for section in section_data['segments']}
@@ -22,6 +22,36 @@ def process_midi_file(midi_path, section_data, drum_mapping):
                     break
 
     return section_counts, existing_drums
+
+def process_midi_file_combined(midi_path, section_data, drum_mapping, all_section_counts, all_existing_drums):
+    visualizer = DrumMidiVisualizer(midi_path)
+    drum_events = visualizer.get_drum_events()
+
+    existing_drums = set()
+    section_counts = {'intro': {}, 'drop': {}, 'break': {}, 'outro': {}}
+
+    for note, event in drum_events.items():
+        drum_name = event['name']
+        existing_drums.add(drum_name)
+        for time in event['times']:
+            for section in section_data['segments']:
+                if section['start'] <= time < section['end']:
+                    if drum_name in section_counts[section['label']]:
+                        section_counts[section['label']][drum_name] += 1
+                    else:
+                        section_counts[section['label']][drum_name] = 1
+                    break
+
+    for section, counts in section_counts.items():
+        if section not in all_section_counts:
+            all_section_counts[section] = {}
+        for drum, count in counts.items():
+            if drum in all_section_counts[section]:
+                all_section_counts[section][drum] += count
+            else:
+                all_section_counts[section][drum] = count
+
+    all_existing_drums.update(existing_drums)
 
 def plot_drum_section_counts(song_name, section_counts, existing_drums):
     num_sections = len(section_counts)
@@ -43,9 +73,12 @@ def plot_drum_section_counts(song_name, section_counts, existing_drums):
     plt.tight_layout()
     plt.show()
 
-def main():
+def main(process_mode):
     json_directory = "../data/demo/allin1_demo/"
     midi_directory = "../data/demo/midi_demo/"
+
+    all_section_counts = {'intro': {}, 'drop': {}, 'break': {}, 'outro': {}}
+    all_existing_drums = set()
 
     for root, dirs, files in os.walk(json_directory):
         for file in files:
@@ -58,9 +91,33 @@ def main():
                 song_name = os.path.splitext(file)[0]
                 print(f"Processing song: {song_name}")
                 section_data = load_section_data(json_path)
-                visualizer = DrumMidiVisualizer(midi_path)
-                section_counts, existing_drums = process_midi_file(midi_path, section_data, visualizer.drum_mapping)
-                plot_drum_section_counts(song_name, section_counts, existing_drums)
+
+                if process_mode == 'single':
+                    section_counts, existing_drums = process_midi_file_single(midi_path, section_data, DrumMidiVisualizer(midi_path).drum_mapping)
+                    plot_drum_section_counts(song_name, section_counts, existing_drums)
+                elif process_mode == 'combined':
+                    process_midi_file_combined(midi_path, section_data, DrumMidiVisualizer(midi_path).drum_mapping, all_section_counts, all_existing_drums)
+
+    if process_mode == 'combined':
+        num_sections = len(all_section_counts)
+        num_cols = 2
+        num_rows = np.ceil(num_sections / num_cols).astype(int)
+        fig, axes = plt.subplots(num_rows, num_cols, figsize=(12, 6 * num_rows))
+        axes = axes.flatten()
+
+        for i, (section, counts) in enumerate(all_section_counts.items()):
+            if i < len(axes):
+                ax = axes[i]
+                filtered_counts = {k: v for k, v in counts.items() if k in all_existing_drums}
+                ax.bar(range(len(filtered_counts)), list(filtered_counts.values()), tick_label=list(filtered_counts.keys()))
+                ax.set_title(f"All Songs - {section} Section")
+                ax.set_xlabel("Drum Elements")
+                ax.set_ylabel("Counts")
+                ax.tick_params(axis='x', rotation=45)
+
+        plt.tight_layout()
+        plt.show()
 
 if __name__ == "__main__":
-    main()
+    process_mode = 'combined'  # 'single' or 'combined'
+    main(process_mode)
