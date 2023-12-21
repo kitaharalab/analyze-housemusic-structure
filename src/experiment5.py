cc1658d2c7836ed924996c0adcbbc1bd796e2a5d
Jtwulf <jtwulf1005@icloud.com>
2023-12-21 23:54:41 +0900

    experiment5 complete (tmp)

---
+++ src/experiment5.py
@@ -0,0 +1,32 @@
+from external_libraries import *
+from modules import *
+
+def load_section_data(json_path: str):
+    with open(json_path, 'r') as file:
+        data = json.load(file)
+    return data
+
+def main():
+    midi_directory = "../data/demo/midi_demo/"
+    json_directory = "../data/demo/allin1_demo/"
+
+    for root, dirs, files in os.walk(midi_directory):
+        for file in files:
+            if file.endswith(".mid"):
+                midi_path = os.path.join(root, file)
+                song_name = os.path.splitext(file)[0]
+                print(f"Processing song: {song_name}")
+
+                # Load JSON data for the current song
+                json_path = os.path.join(json_directory, f"{song_name}.json")
+                section_data = load_section_data(json_path)
+
+                drum = Drum(midi_path)
+                events = drum.get_drum_events()
+                pattern_changes = drum.detect_pattern_changes(events)
+                print(f"Pattern Changes for {song_name}:\n {pattern_changes}")
+
+                drum.plot_drum_with_pattern_and_segments(song_name, events, pattern_changes, section_data)
+
+if __name__ == "__main__":
+    main()
