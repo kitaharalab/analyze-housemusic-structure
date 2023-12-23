91aaf0afbf3f668652c3b3aade0d9fcc473c6055
Jtwulf <jtwulf1005@icloud.com>
2023-12-23 21:05:29 +0900

    agjust allin1 script

--- src/scripts/allin1.sh
+++ src/scripts/allin1.sh
@@ -1,11 +1,16 @@
 #!/bin/bash
 
-INPUT_DIR="/path/to/input/directory"
-OUTPUT_DIR="/path/to/output/directory"
+INPUT_DIR="/Users/justinwulf/Documents/justin/school/main/data/prod/songs/wav"
+OUTPUT_DIR="/Users/justinwulf/Documents/justin/school/main/data/prod/allin1"
+BACKUP_DIR="${INPUT_DIR}_backup"
+
+cp -r "$INPUT_DIR" "$BACKUP_DIR"
 
 mkdir -p "$OUTPUT_DIR"
 
-find "$INPUT_DIR" -type f -name "*.wav" | while read filename; do
+find "$INPUT_DIR" -type f -name "*.wav" | while IFS= read -r filename; do
     allin1 -o "$OUTPUT_DIR" "$filename"
+    if [ $? -eq 0 ]; then
+        rm "$filename"
+    fi
 done
-
