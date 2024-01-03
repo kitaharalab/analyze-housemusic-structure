0f13ee356608bb993f2bb6c44b5473f3082237ff
Jtwulf <jtwulf1005@icloud.com>
2024-01-04 00:24:32 +0900

    update results to prod

--- src/experiment1.py
+++ src/experiment1.py
@@ -75,15 +75,15 @@ def process_files(json_directory, song_directory, freq, allin1, all_section_aver
                 process_file(json_path, song_directory, freq, all_section_averages, allin1)
 
 def main(process_mode):
-    song_directory = const.DEMO_SONG_DIRECTORY
-    json_directory = const.DEMO_JSON_DIRECTORY
+    song_directory = const.PROD_SONG_DIRECTORY
+    json_directory = const.PROD_JSON_DIRECTORY
     freq = Frequency()
     allin1 = Allin1()
     all_section_averages = {'intro': [], 'drop': [], 'break': [], 'outro': []}
 
     process_files(json_directory, song_directory, freq, allin1, all_section_averages)
 
-    if process_mode == 'var':
+    if process_mode == 'bar':
         plot_bar_graph(all_section_averages)
     elif process_mode == 'box':
         plot_box_plot(all_section_averages)
@@ -91,5 +91,5 @@ def main(process_mode):
         plot_violin_plot(all_section_averages)
 
 if __name__ == "__main__":
-    process_mode = 'violin'  # 'var', 'box', or 'violin'
+    process_mode = 'box'  # 'var', 'box', or 'violin'
     main(process_mode)
