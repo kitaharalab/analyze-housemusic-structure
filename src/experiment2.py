963214df6c601d8498e46fef4587ad34c66f6a45
Jtwulf <jtwulf1005@icloud.com>
2024-01-08 00:18:56 +0900

    combined box plot with mean

--- src/experiment2.py
+++ src/experiment2.py
@@ -246,5 +246,5 @@ def main(process_mode):
 
 
 if __name__ == "__main__":
-    process_mode = 'combined_bar'  # 'bar' | 'combined_bar' | 'box' | 'combined_box' | 'violin' | 'combined_violin' | 'rms_plot'
+    process_mode = 'combined_box'  # 'bar' | 'combined_bar' | 'box' | 'combined_box' | 'violin' | 'combined_violin' | 'rms_plot'
     main(process_mode)
